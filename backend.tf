686817713be2ccf76a508e2fad7815bfcd1e8964
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-09-11 20:28:33 +0800

    Change backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce3-tfstate-bucket"
+    bucket = "sctp-ce3-tfstate-bucket-1"
     key    = "jazeel-ssm-demo.tfstate" #Change the value  of this to yourname-ssm.tfstate for  example
     region = "us-east-1"
   }
